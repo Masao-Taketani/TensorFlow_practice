2d7911204bbb58464d8edbb5d85486510f66c38f
Masao-Taketani <taket003@umn.edu>
2019-12-02 22:58:01 +0900

    tokenization_sentencepiece.py is done

--- 1.x/BERT/tokenization_sentencepiece.py
+++ 1.x/BERT/tokenization_sentencepiece.py
@@ -62,3 +62,91 @@ def convert_to_unicode(text):
 		return text.decode("utf-8", "strict")
 	else:
 		raise ValueError("Unsupported string type: %s" % (type(text)))
+
+
+def printable_text(text):
+
+	if isinstance(text, str):
+		return text
+	elif isinstance(text, bytes):
+		return text.decode("utf-8", "strict")
+	else:
+		raise ValueError("Unsupported string type: %s" % (type(text)))
+
+# loads a vocab file into a dict
+def load_vocab(vocab_file):
+
+	vocab = collections.OrderedDict()
+	index = 0
+	with tf.gfile.GFile(vocab_file, "r") as reader:
+		while True:
+			token = convert_to_unicode(reader.readline())
+			if not token:
+				break
+			token, _ = token.split("\t")
+			token = token.strip()
+			vocab[token] = index
+			index += 1
+	return vocab
+
+
+def convert_by_vocab(vocab, items, unk_info):
+
+	output = []
+	for item in items:
+		if item in vocab:
+			output.append(vocab[item])
+		else:
+			output.append(unk_info)
+	return output
+
+
+def convert_tokens_to_ids(vocab, tokens):
+
+	return convert_by_vocab(vocab, tokens, unk_info=0)
+
+
+def convert_ids_to_tokens(inv_vocab, ids):
+
+	return convert_by_vocab(inv_vocab, ids, unk_info="<unk>")
+
+
+class FullTokenizer(object):
+
+	def __init__(self, model_file, vocab_file, do_lower_case=True):
+		self.tokenizer = SentencePieceTokenizer(model_file, do_lower_case=do_lower_case)
+		self.vocab = load_vocab(vocab_file)
+		self.inv_vocab = {v: k for k, v in self.vocab.items()}
+
+	def tokenize(self, text):
+		split_tokens = self.tokenizer.tokenize(text)
+		return split_tokens
+
+	def convert_tokens_to_ids(self, tokens):
+
+		return convert_by_vocab(self.vocab, tokens, unk_info=0)
+
+	def convert_ids_to_tokens(self, ids):
+
+		return convert_by_vocab(self.vocab, ids, unk_info="<unk>")
+
+
+class SentencePieceTokenizer(object):
+
+	def __init__(self, model_file=None, do_lower_case=True):
+
+		self.tokenizer = sp.SentencePieceProcessor()
+		if self.tokenizer.load(model_file):
+			print("Loaded a trained SentencePiece model.")
+		else:
+			print("You have to give a path of trained SentencePiece model.")
+			sys.exit(1)
+		self.do_lower_case = do_lower_case
+
+	def tokenize(self, text):
+
+		text = convert_to_unicode(text)
+		if self.do_lower_case:
+			text = text.lower()
+		output_tokens = self.tokenizer.EncodeAsPieces(text)
+		return output_tokens
