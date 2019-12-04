d7655bb849ca9c33a3919a4c1de7a612041440ee
Masao-Taketani <taket003@umn.edu>
2019-12-04 23:58:38 +0900

    done implementing 'write_instance_to_example_files' func in create_pretraining_data.py

--- 1.x/BERT/create_pretraining_data.py
+++ 1.x/BERT/create_pretraining_data.py
@@ -39,3 +39,133 @@ flags.DEFINE_float("short_seq_prob", 0.1,
 					"maximum length.")
 
 
+class TrainingInstance(object):
+
+	def __init__(self, tokens, segment_ids, masked_lm_positions, masked_lm_labels,
+				 is_random_next):
+
+		self.tokens = tokens
+		self.segment_ids = segment_ids
+		self.is_random_next = is_random_next
+		self.masked_lm_positions = masked_lm_positions
+		self.masked_lm_labels = masked_lm_labels
+
+	def __str__(self):
+		s = ""
+		s += "tokens: %s\n" % (" ".join(
+			[tokenization.printable_text(x) for x in self.tokens]))
+		s += "segment_ids: %s\n" % (" ".join(
+			[str(x) for x in self.segment_ids]))
+		s += "is_random_next: %s\n" % self.is_random_next
+		s += "masked_lm_positions: %s\n" % (" ".join(
+			[str(x) for x in self.masked_lm_positions]))
+		s += "masked_lm_labels: %s\n" % (" ".join(
+			[tokenization.printable_text(x) for x in self.masked_lm_labels]))
+		s += "\n"
+		return s
+
+	def __repr__(self):
+		return self.__str__()
+
+
+def write_instance_to_example_files(instances, tokenizer, max_seq_length,
+									max_predictions_per_seq, output_files):
+	# Create TF example files from 'TrainingInstance's'.
+	writers = []
+	for output_file in output_files:
+		# TFRecordWriter: A class to write records to a TFRecords file.
+		writers.append(tf.python_io.TFRecordWriter(output_file))
+
+	writer_index = 0
+	total_written = 0
+	for (inst_index, instance) in enumerate(instances):
+		input_ids = tokenizer.convert_tokens_to_ids(instance.tokens)
+		input_mask = [1] * len(input_ids)
+		segment_ids = list(instance.segment_ids)
+		assert len(input_ids) <= max_seq_length
+
+		while len(input_ids) < max_seq_length:
+			input_ids.append(0)
+			input_mask.append(0)
+			segment_ids.append(0)
+
+		assert len(input_ids) == max_seq_length
+		assert len(input_mask) == max_seq_length
+		assert len(segment_ids) == max_seq_length
+
+		masked_lm_positions = list(instance.masked_lm_positions)
+		masked_lm_ids = tokenizer.convert_tokens_to_ids(instance.masked_lm_labels)
+		masked_lm_weights = [1.0] * len(masked_lm_ids)
+
+		while len(masked_lm_positions) < max_predictions_per_seq:
+			masked_lm_positions.append(0)
+			masked_lm_ids.append(0)
+			masked_lm_weights.append(0.0)
+
+		next_sentence_label = 1 if instance.is_random_next else 0
+
+		features = collections.OrderedDict()
+		features["input_ids"] = create_int_features(input_ids)
+		features["input_mask"] = create_int_features(input_mask)
+		features["segment_ids"] = create_int_features(segment_ids)
+		features["masked_lm_positions"] = create_int_features(masked_lm_positions)
+		features["masked_lm_ids"] = create_int_features(masked_lm_ids)
+		features["masked_lm_weights"] = create_float_features(masked_lm_weights)
+		features["next_sentence_label"] = create_int_features([next_sentence_label])
+		"""データの読み込みを効率的にするには、データをシリアライズし、連続的に読み込めるファイルのセット
+		（各ファイルは 100-200MB）に保存することが有効です。データをネットワーク経由で流そうとする場合には、
+		特にそうです。また、データの前処理をキャッシングする際にも役立ちます。
+		TFRecord 形式は、バイナリレコードのシリーズを保存するための単純な形式です。"""
+		tf_example = tf.train.Example(features=tf.train.Features(feature=features))
+		
+		"""tf.Example 用のデータ型
+		基本的には tf.Example は {"string": tf.train.Feature} というマッピングです。
+
+		tf.train.Feature メッセージ型は次の3つの型のうち1つをとることができます。（.proto file を参照）このほかの一般的なデータ型のほとんどは、強制的にこれらのうちの1つにすること可能です。
+
+		tf.train.BytesList (次の型のデータを扱うことが可能)
+
+			string
+			byte
+		tf.train.FloatList (次の型のデータを扱うことが可能)
+
+			float (float32)
+			double (float64)
+		tf.train.Int64List (次の型のデータを扱うことが可能)
+
+			bool
+			enum
+			int32
+			uint32
+			int64
+			uint64"""
+		# 主要なメッセージはすべて .SerializeToString を使ってバイナリ文字列にシリアライズすることができます。
+		# TFRecordWriter instance writes a string record to the file.
+		writers[writer_index].write(tf_example.SerializeToString())
+
+		writer_index = (writer_index + 1) % len(writers)
+		total_written += 1
+
+		if inst_index < 20:
+			tf.logging.info("*** Example ***")
+			tf.logging.info("token: %s" % " ".join(
+				[tokenizer.printable_text(x) for x in instance.tokens]))
+
+			for feature_name in features.keys():
+				feature = features[feature_name]
+				values = []
+				if feature.int64_list.value:
+					values = feature.int64_list.value
+				elif feature.float_list.value:
+					values = feature.float_list.value
+				tf.logging.info(
+					"%s: %s" % (feature_name, " ".join([str(x) for x in values])))
+
+	for writer in writers:
+		writer.close()
+
+	tf.logging.info("Wrote %d total instances", total_written)
+
+
+# if you use functions below, you can convert values into data types which are
+# compatible to tf.Example
