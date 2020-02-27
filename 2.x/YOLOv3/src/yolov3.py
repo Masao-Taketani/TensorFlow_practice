be4cc6b9194fc782e6b93ea1a05552d5dab406fd
Masao-Taketani <taket003@umn.edu>
2020-02-27 20:07:50 +0900

    decode func is done

--- 2.x/YOLOv3/src/yolov3.py
+++ 2.x/YOLOv3/src/yolov3.py
@@ -12,6 +12,7 @@ ANCHORS = utils.get_anchors(cfg.YOLO.ANCHORS)
 STRIDES = np.array(cfg.YOLO.STRIDES)
 IOU_LOSS_THRESH = cfg.YOLO.IOU_LOSS_THRESH
 
+
 def YOLOv3(input_layer):
     scale1, scale2, scale3 = backbone.darknet53(input_layer)
 
@@ -64,3 +65,113 @@ def YOLOv3(input_layer):
                                    bn=False)
 
     return [conv_sbbox, conv_mbbox, conv_lbbox]
+
+
+def decode(conv_output, i=0):
+    # tf.shape return a tensor
+    conv_shape = tf.shape(conv_output)
+    batch_size = conv_shape[0]
+    output_size = conv_shape[1]
+
+    conv_output = tf.reshape(conv_output,
+                             (batch_size,
+                             output_size,
+                             output_size,
+                             3,
+                             5 + NUM_CLASSES))
+
+    conv_raw_txty = conv_output[:,:,:,:,0:2]
+    conv_raw_twth = conv_output[:,:,:,:,2:4]
+    conv_raw_conf = conv_output[:,:,:,:,4:5]
+    conv_raw_cls_prob = conv_output[:,:,:,:,5:]
+
+    """
+    For cy:
+        inside of tf.range(...):
+            from [0,1,2,..., len(output_size)-1]
+            to [[0], [1], [2], ..., [len(output_size)-1]]
+
+        inside of tf.tile(...):
+            from [[0], [1], [2], ..., [len(output_size)-1]]
+            to [[0,0,...,0], [1,1,...,1], ...,]
+
+        output shape:
+            [len(output_size), len(output_size)]
+    """
+    cy = tf.tile(tf.range(output_size,
+                         dtype=tf.int32)[:, tf.newaxis],
+                [1, output_size])
+
+    """
+    For cx:
+        inside of tf.range(...):
+            from [0,1,2,..., len(output_size)-1]
+            to [[0,1,2,..., len(output_size)-1]]
+
+        inside of tf.tile(...):
+            from [[0,1,2,..., len(output_size)-1]]
+            to [[0,1,2,..., len(output_size)-1], [0,1,2,..., len(output_size)-1], ...,]
+
+        output shape:
+            [len(output_size), len(output_size)]
+    """
+    cx = tf.tile(tf.range(output_size,
+                         dtype=tf.int32)[tf.newaxis, :],
+                [output_size, 1])
+    # 1st channel: grid of cx
+    # 2nd channel: grid of cy
+    cxcy_grid = tf.concat([cx[:, :, tf.newaxis],
+                         cy[:, :, tf.newaxis]],
+                        axis=-1)
+    cxcy_grid = tf.tile(cxcy_grid[tf.newaxis, :, :, tf.newaxis, :],
+                      [batch_size, 1, 1, 3, 1])
+    cxcy_grid = tf.cast(cxcy_grid, tf.float32)
+
+    """
+    For x and y:
+        bx = σ(tx) + cx
+        by = σ(ty) + cy
+    For w and h:
+        bw = pw * exp(tw)
+        bh = ph * exp(th)
+    """
+    """
+    shape of pred_xy:
+        [batch_size,
+         output_size,
+         output_size,
+         num_of_priors_per_scale,
+         len([x,y])]
+    """                    ]
+    pred_xy = (tf.sigmoid(conv_raw_txty) + cxcy_grid) * STRIDES[i]
+    """
+    ANCHORS (3,3,2):
+        np.array([[[p1_x, p1_y],
+                   [p2_x, p2_y],
+                   [p3_x, p3_y]],
+                  [[p4_x, p4_y],
+                   [p5_x, p5_y],
+                   [p6_x, p6_y]],
+                  [[p7_x, p7_y],
+                   [p8_x, p8_y],
+                   [p9_x, p9_y]]])
+    """
+    pred_wh = (ANCHORS[i] * STRIDES[i]) * tf.exp(conv_raw_twth)
+    # 1st channel: prediction of x
+    # 2nd channel: prediction of y
+    # 3rd channel: prediction of w
+    # 4th channel: prediction of h
+    pred_xywh = tf.concat([pred_xy, pred_wh], axis=-1)
+    # num of depths: 1
+    pred_conf = tf.sigmoid(conv_raw_conf)
+    # num of depths: class size
+    pred_prob = tf.sigmoid(conv_raw_cls_prob)
+    """
+    shape of return tensor:
+        [batch_size,
+         output_size,
+         output_size,
+         num_of_priors_per_scale,
+         len([x,y,w,h])+1(=conf)+c+class_size]
+    """     
+    return tf.concat([pred_xywh, pred_conf, pred_prob], axis=-1)
